9ff5e423f279da742241805757b849a65ce9ea03
huangguodong <15717288268@163.com>
2024-10-28 15:39:21 +0800

    完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,81 @@
 <script setup>
 import { ref } from 'vue'
+import { Swipe, SwipeItem } from 'vant';
+import { showImagePreview } from 'vant';
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+const show = ref(false)
+const handleClick = (item) => {
+  console.log(item)
+}
+const showMoney = () => {
+  show.value = true
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="container">
+    <div class="swipe-box">
+      <van-swipe :autoplay="3000" lazy-render>
+      <van-swipe-item @click="handleClick">
+        <img class="img-box" src="../assets/601730098801_.pic.jpg" />
+      </van-swipe-item>
+      <van-swipe-item>
+        <img class="img-box" src="../assets/611730098802_.pic.jpg" />
+      </van-swipe-item>
+      <van-swipe-item>
+        <img class="img-box" src="../assets/621730098803_.pic.jpg" />
+      </van-swipe-item>
+      <van-swipe-item>
+        <img class="img-box" src="../assets/631730098806_.pic.jpg" />
+      </van-swipe-item>
+      <van-swipe-item>
+        <img class="img-box" src="../assets/641730098808_.pic.jpg" />
+      </van-swipe-item>
+    </van-swipe>
+    </div>
+    <div class="touxiang">
+      <img class="touxiangImg" src="../assets/651730098811_.pic.jpg" alt="">
+    </div>
+    <div class="info">
+      <van-cell-group>
+        <van-cell title="姓名" value="刘彤怡" />
+        <van-cell title="性别" value="美女" label="是很美的那种美女" />
+        <van-cell title="年龄" value="22" />
+        <van-cell title="爱好" value="睡觉" />
+        <van-cell title="住址" value="湖北武汉" />
+        <van-cell title="个性" value="懒" />
+      </van-cell-group>
+    </div>
+    <van-floating-bubble icon="gold-coin-o" @click="showMoney" />
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <van-dialog v-model:show="show">
+  <img style="height: 360px; width: 360px;margin: 0 auto" src="../assets/WechatIMG66.jpg" />
+</van-dialog>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.container {
+  padding: 16px;
+}
+.swipe-box {
+  height: 200px;
+  width: 100%;
+}
+.img-box {
+  height: 200px;
+  width: 100%;
+}
+.touxiang {
+  position: relative;
+}
+.touxiangImg {
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  position: absolute;
+  top: -50px;
+}
+.info {
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'vant/lib/index.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import Vant from 'vant'
+const app = createApp(App)
+app.use(Vant).mount('#app')
